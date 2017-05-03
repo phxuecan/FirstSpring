fc8033efafcd92724d67baae8aa09d51b24e08ed
lihq8 <lhq@putao.cn>
2017-05-03 15:11:31 +0800

    spring boot 错误处理

--- SpringBootHelloWorld/src/main/java/com/leehq/spring/Application.java
+++ SpringBootHelloWorld/src/main/java/com/leehq/spring/Application.java
@@ -2,7 +2,6 @@ package com.leehq.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
@@ -27,8 +26,8 @@ public class Application
     {
         return configurableEmbeddedServletContainer ->
         {
-            configurableEmbeddedServletContainer.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error/400.html"));
-            configurableEmbeddedServletContainer.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404.html"));
+            configurableEmbeddedServletContainer.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/400.html"));
+            configurableEmbeddedServletContainer.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
         };
     }
 }
--- SpringBootHelloWorld/src/main/java/com/leehq/spring/GlobalExceptionHandler.java
+++ SpringBootHelloWorld/src/main/java/com/leehq/spring/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.leehq.spring;
 
+import com.leehq.spring.contacts.ContactsController;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * Created by putao_lhq on 17-4-27.
  */
-@ControllerAdvice
+@ControllerAdvice(basePackageClasses = ContactsController.class)
 public class GlobalExceptionHandler
 {
-    public static final String DEFAULT_ERROR_VIEW = "error";
+    public static final String DEFAULT_ERROR_VIEW = "error_advice";
 
     @ExceptionHandler(value = Exception.class)
     public ModelAndView defualtErrorHandler(HttpServletRequest req, Exception e) throws Exception
     {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e);
+        modelAndView.addObject("message", e.getMessage());
         modelAndView.addObject("url", req.getRequestURI());
         modelAndView.setViewName(DEFAULT_ERROR_VIEW);
         return modelAndView;
