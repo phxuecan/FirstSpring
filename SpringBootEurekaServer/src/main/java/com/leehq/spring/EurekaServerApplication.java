a732612fefc83b43c0fa2829d051263a2e9cdfdf
lihq8 <lhq@putao.cn>
2017-05-15 18:11:10 +0800

    eureka

---
+++ SpringBootEurekaServer/src/main/java/com/leehq/spring/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.leehq.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by putao_lhq on 17-5-15.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
